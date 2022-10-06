1ebf8680344448a6635598398e406d58d090b13b
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-10-06 20:49:05 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from './Home/Home';
+import { Movies } from './Movies/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+      </Routes>
     </div>
   );
 };
